8259584ed9e0ece71c800c5996cfb036f86937ea
paixaoDev <carlos.vspaixao@gmail.com>
2024-06-11 12:07:45 -0300

    (feat): updated design system theme:  - added shapes  - added colors from material builder  - added type from material builder

---
+++ app/src/main/java/com/paixao/dev/mobile_design_system/ui/theme/Shapes.kt
@@ -0,0 +1,13 @@
+package com.paixao.dev.mobile_design_system.ui.theme
+
+import androidx.compose.material3.ShapeDefaults
+import androidx.compose.material3.Shapes
+
+
+val designSystemShapes = Shapes(
+    extraSmall = ShapeDefaults.ExtraSmall,
+    small = ShapeDefaults.Small,
+    medium = ShapeDefaults.Medium,
+    large = ShapeDefaults.Large,
+    extraLarge = ShapeDefaults.ExtraLarge
+)
