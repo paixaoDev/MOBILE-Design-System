b78a1cd06f43fb12a84aa25d4b50af8cb27dbb29
paixaoDev <carlos.vspaixao@gmail.com>
2024-06-11 16:25:08 -0300

    (feat): created component CardExhibition:  - main composable with default values  - private composable with behaviors

--- app/src/main/java/com/paixao/dev/mobile_design_system/components/CardExhibition.kt
+++ app/src/main/java/com/paixao/dev/mobile_design_system/components/CardExhibition.kt
@@ -1,37 +1,24 @@
 package com.paixao.dev.mobile_design_system.components
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberUpdatedState
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.DefaultAlpha
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import com.paixao.dev.mobile_design_system.R
 import com.paixao.dev.mobile_design_system.ui.theme.MobileDesignSystemTheme
 /**
  *  A card that's can exhibit a [Image] with a description [Text]. This can exhibit just with a
