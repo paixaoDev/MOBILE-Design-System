97dabef9c96e152ead25b7fde8699e7312c5a59e
paixaoDev <carlos.vspaixao@gmail.com>
2024-07-03 12:36:15 -0300

    (feat): created component CardEditText:  - removed unused import

--- app/src/main/java/com/paixao/dev/mobile_design_system/components/CardEditText.kt
+++ app/src/main/java/com/paixao/dev/mobile_design_system/components/CardEditText.kt
@@ -33,7 +33,7 @@ import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.paixao.dev.mobile_design_system.R
+
 import java.util.Locale
 
 @Composable
